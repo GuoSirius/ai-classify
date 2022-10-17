c6dbb80e4a48760dd0546f766271f952bdcf8b9d
郭之存 <guoplc@163.com>
2022-10-17 18:44:01 +0800

    OK

--- train/constant.js
+++ train/constant.js
@@ -1,10 +1,17 @@
-const TEST_PATH = 'data/test'
-const TRAIN_PATH = 'data/train'
+const path = require('path')
 
-const OPTPUT_PATH = 'output'
+const TEST_PATH = path.resolve('data/test')
+const TRAIN_PATH = path.resolve('data/train')
+
+const OPTPUT_PATH = path.resolve('output')
+
+// const MOBILE_NET_URL = path.resolve('train/mobile-net.json')
+const MOBILE_NET_URL = 'http://127.0.0.1:8083/train/mobile-net.json'
+// const MOBILE_NET_URL = 'http://ai-sample.oss-cn-hangzhou.aliyuncs.com/pipcook/models/mobilenet/web_model/model.json'
 
 exports = module.exports = {
   TEST_PATH,
   TRAIN_PATH,
-  OPTPUT_PATH
+  OPTPUT_PATH,
+  MOBILE_NET_URL
 }
--- train/index.js
+++ train/index.js
@@ -1,4 +1,6 @@
-const { TRAIN_PATH, OPTPUT_PATH } = require('./constant')
+const tf = require('@tensorflow/tfjs-node')
+
+const { TRAIN_PATH, OPTPUT_PATH, MOBILE_NET_URL } = require('./constant')
 
 const loadData = require('./data')
 
@@ -7,6 +9,9 @@ main()
 async function main() {
   // 加载数据
   loadData(TRAIN_PATH, OPTPUT_PATH)
+
+  const mobileNet = await tf.loadLayersModel(MOBILE_NET_URL)
+  mobileNet.summary()
 
   // 定义模型
   // 训练模型
