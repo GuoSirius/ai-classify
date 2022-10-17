a6ddd01405ec75eef2e5ae97b9c5694bc62e400a
郭之存 <guoplc@163.com>
2022-10-17 12:59:19 +0800

    initialize

---
+++ train/data.js
@@ -0,0 +1,9 @@
+main()
+
+async function main() {
+  // 加载数据
+  // 定义模型
+  // 训练模型
+}
+
+exports = module.exports = main
---
+++ train/index.js
@@ -0,0 +1,11 @@
+const loadData = require('./data')
+
+main()
+
+async function main() {
+  // 加载数据
+  loadData()
+
+  // 定义模型
+  // 训练模型
+}
